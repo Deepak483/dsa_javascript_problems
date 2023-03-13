9b89ff9d3c0eb56230ba0c7b0bdc8ad16fa9d238
Deepak Tiwari <ainutiwari483@gmail.com>
2023-03-14 01:10:50 +0530

    rotate image complete

--- rotateImage.js
+++ rotateImage.js
@@ -14,15 +14,22 @@ function rotateImage(n, matrix) {
    *9 6 3 (2,2) (1,2) (0,2)
    */
   let matrix1 = [];
-  for (let i = 0; i < n; i++){
-    matrix1.push(new Array(n).fill(0));
-  }
+  // for (let i = 0; i < n; i++){
+  //   matrix1.push(new Array(n).fill(0));
+  // }
+  let arr = [];
   let temp;
   console.log(matrix1);
   for (let j = 0; j < n; j++){
     for (let i = n - 1; i >= 0; i--){
-      temp = matrix[i][j];
+      // console.log(matrix[i
+      arr.push(matrix[i][j]);
     }
+  }
+  console.log(arr);
+  console.log(matrix1);
+  for (let i = 0; i < n; i++){
+    matrix1.push(new Array(n).fill(arr[i]));
   }
   console.log(matrix1);
   return matrix1;
