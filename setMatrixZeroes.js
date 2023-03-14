7c03d6f3c9ed6a0c37d0c9bcd35d8ee42a91b007
Deepak Tiwari <ainutiwari483@gmail.com>
2023-03-15 01:24:04 +0530

    changes made

--- setMatrixZeroes.js
+++ setMatrixZeroes.js
@@ -24,45 +24,47 @@ Sample Output 2
  */
 function setMatrixZeroes(matrix) {
   //best approach
-  let m = matrix.length; //row
-  let n = matrix[0].length; //column
+  let m = matrix.length;//row
+  let n = matrix[0].length;//column
   let x = 1;
   let y = 1;
-  for (let j = 0; j < n; j++) {
-    if (matrix[0][j] == 0) x = 0;
+  for (let j = 0; j < n; j++){
+    if (matrix[0][j] == 0)
+      x = 0;
   }
-  for (let i = 0; i < m; i++) {
-    if (matrix[i][0] == 0) y = 0;
+  for (let i = 0; i < m; i++){
+    if (matrix[i][0] == 0)
+      y = 0;      
   }
-  for (let i = 1; i < m; i++) {
-    for (let j = 1; j < n; j++) {
+  for (let i = 1; i < m; i++){
+    for (let j = 1; j < n; j++){
       if (matrix[i][j] == 0) {
         matrix[i][0] = 0;
         matrix[0][j] = 0;
       }
     }
   }
-  for (let j = 1; j < n; j++) {
+  for(let j = 1; j < n; j++) {
     if (matrix[0][j] == 0) {
-      for (let i = 1; i < m; i++) {
+      for (let i = 1; i < m; i++){
         matrix[i][j] = 0;
       }
     }
   }
-  for (let i = 1; i < n; i++) {
+  for(let i = 1; i < n; i++) {
     if (matrix[i][0] == 0) {
-      for (let j = 0; j < n; i++) {
+      for (let j = 0; j < n; i++){
         matrix[i][j] = 0;
       }
     }
   }
   if (y == 0) {
-    for (let i = 0; i < m; i++) {
+    for (let i = 0; i < m; i++){
       matrix[i][0] = 0;
     }
   }
   if (x == 0) {
-    for (let j = 0; j < n; j++) {
+    for (let j = 0; j < n; j++){
       matrix[0][j] = 0;
     }
   }
@@ -75,4 +77,4 @@ let m = [
   [1, 1, 1],
 ];
 const result = setMatrixZeroes(m);
-console.log(result);
+console.log(result);  
